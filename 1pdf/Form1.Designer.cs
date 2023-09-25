8a82f9a206176b15a5b9db4a3a53bb98530e4bfc
jhbelalc <jhbelalc@gmail.com>
2023-09-25 16:12:14 -0500

    Agregar archivos de proyecto.

---
+++ 1pdf/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace _1pdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFiles = new System.Windows.Forms.Button();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnArchivo = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAbajo = new System.Windows.Forms.Button();
+            this.btnArriba = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerar.Image = global::_1pdf.Properties.Resources.Document;
+            this.btnGenerar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGenerar.Location = new System.Drawing.Point(718, 293);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(150, 50);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Convertir 1 PDF";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSelectFiles
+            // 
+            this.btnSelectFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectFiles.Image = global::_1pdf.Properties.Resources.add;
+            this.btnSelectFiles.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSelectFiles.Location = new System.Drawing.Point(554, 12);
+            this.btnSelectFiles.Name = "btnSelectFiles";
+            this.btnSelectFiles.Size = new System.Drawing.Size(150, 50);
+            this.btnSelectFiles.TabIndex = 2;
+            this.btnSelectFiles.Text = "Seleccionar o arrastre y suelte";
+            this.btnSelectFiles.UseVisualStyleBackColor = true;
+            this.btnSelectFiles.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtnombre.Location = new System.Drawing.Point(554, 94);
+            this.txtnombre.Multiline = true;
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(314, 81);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblNombre.Location = new System.Drawing.Point(554, 76);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(234, 15);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre nuevo PDF, clic AQUÍ para sugerir.";
+            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
+            // 
+            // btnArchivo
+            // 
+            this.btnArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnArchivo.Location = new System.Drawing.Point(844, 181);
+            this.btnArchivo.Name = "btnArchivo";
+            this.btnArchivo.Size = new System.Drawing.Size(24, 23);
+            this.btnArchivo.TabIndex = 5;
+            this.btnArchivo.Text = "...";
+            this.btnArchivo.UseVisualStyleBackColor = true;
+            this.btnArchivo.Click += new System.EventHandler(this.btnArchivo_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(536, 334);
+            this.listBox1.TabIndex = 6;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.Image = global::_1pdf.Properties.Resources.bad;
+            this.btnRemove.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRemove.Location = new System.Drawing.Point(554, 293);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(150, 50);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "Borrar";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAbajo
+            // 
+            this.btnAbajo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAbajo.Image = global::_1pdf.Properties.Resources.Down;
+            this.btnAbajo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAbajo.Location = new System.Drawing.Point(554, 241);
+            this.btnAbajo.Name = "btnAbajo";
+            this.btnAbajo.Size = new System.Drawing.Size(150, 50);
+            this.btnAbajo.TabIndex = 7;
+            this.btnAbajo.Text = "Abajo";
+            this.btnAbajo.UseVisualStyleBackColor = true;
+            this.btnAbajo.Click += new System.EventHandler(this.btnAbajo_Click);
+            // 
+            // btnArriba
+            // 
+            this.btnArriba.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnArriba.Image = global::_1pdf.Properties.Resources.Up;
+            this.btnArriba.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnArriba.Location = new System.Drawing.Point(554, 188);
+            this.btnArriba.Name = "btnArriba";
+            this.btnArriba.Size = new System.Drawing.Size(150, 50);
+            this.btnArriba.TabIndex = 7;
+            this.btnArriba.Text = "Arriba";
+            this.btnArriba.UseVisualStyleBackColor = true;
+            this.btnArriba.Click += new System.EventHandler(this.btnArriba_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 364);
+            this.Controls.Add(this.btnArriba);
+            this.Controls.Add(this.btnAbajo);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnArchivo);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.btnSelectFiles);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.Text = "Unirs PDFs en uno solo. ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSelectFiles;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnArchivo;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAbajo;
+        private System.Windows.Forms.Button btnArriba;
+    }
+}
+
---
+++ 1pdf/Form1.cs
@@ -0,0 +1,194 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Windows.Forms;
+
+namespace _1pdf
+{
+    public partial class Form1 : Form
+    {
+        List<string> fileNames = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.AllowDrop = true;
+            listBox1.DragEnter += ListBox1_DragEnter;
+            listBox1.DragDrop += ListBox1_DragDrop;
+        }
+
+
+        private void ListBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+
+        private void ListBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                bool containsOnlyPDF = files.All(f => Path.GetExtension(f).Equals(".pdf", StringComparison.OrdinalIgnoreCase));
+                if (containsOnlyPDF)
+                {
+                    foreach (string file in files)
+                    {
+                        fileNames.Add(file);
+                        listBox1.Items.Add(file);
+                    }
+                }
+            }
+        }
+
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                fileNames.RemoveAt(listBox1.SelectedIndex);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (listBox1.Items.Count == 0) return;
+            if (string.IsNullOrWhiteSpace(txtnombre.Text))
+            {
+                MessageBox.Show("Debe escoger el nombre del archivo a generar.", "Falta un parámetro para proceder:", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            List<string> inputFiles = new List<string>();
+            inputFiles.AddRange(listBox1.Items.OfType<string>());
+
+            var outputFile = txtnombre.Text; //@"C:\Users\john.belalcazar\Documents\Kantar\Viaje y Alojamiento\Marzo 2023\MED CO-FO-FIN- 0004 - v.3  Legalizacion gastos de Personal.pdf";
+
+            MergePdfDocuments(inputFiles, outputFile);
+            if (File.Exists(outputFile))
+            {
+                MessageBox.Show("El archivo " + outputFile + " ha sido creado con éxito.", "Proceso finalizado de forma correcta.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El archivo " + outputFile + " no pudo ser creado.", "Proceso con errores.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MergePdfDocuments(List<string> inputFiles, string outputFile)
+        {
+            
+            using (var writer = new PdfWriter(outputFile))
+            {
+                using (var document = new PdfDocument(writer))
+                {
+                    var merger = new PdfMerger(document);
+
+                    foreach (var inputFile in inputFiles)
+                    {
+                        using (var reader = new PdfReader(inputFile))
+                        {
+                            var sourceDocument = new PdfDocument(reader);
+                            merger.Merge(sourceDocument, 1, sourceDocument.GetNumberOfPages());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string fileName in openFileDialog.FileNames)
+                    {
+                        if (!fileNames.Contains(fileName))
+                        {
+                            fileNames.Add(fileName);
+                            listBox1.Items.Add(fileName);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private void btnArchivo_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Archivo PDF (*.pdf)|*.pdf";
+            saveFileDialog.Title = "Guardar archivo PDF";
+            saveFileDialog.FileName = "DocumentoCompleto.pdf";
+            DialogResult result = saveFileDialog.ShowDialog();
+
+            // Si el usuario hizo clic en Aceptar en el cuadro de diálogo, guardar el archivo
+            if (result == DialogResult.OK)
+            {
+                txtnombre.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void btnArriba_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index > 0)
+            {
+                string temp = fileNames[index - 1];
+                fileNames[index - 1] = fileNames[index];
+                fileNames[index] = temp;
+                listBox1.Items.Clear();
+                foreach (string file in fileNames)
+                {
+                    listBox1.Items.Add(file);
+                }
+                listBox1.SelectedIndex = index - 1;
+            }
+        }
+
+        private void btnAbajo_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index < listBox1.Items.Count - 1 && index != -1)
+            {
+                string temp = fileNames[index + 1];
+                fileNames[index + 1] = fileNames[index];
+                fileNames[index] = temp;
+                listBox1.Items.Clear();
+                foreach (string file in fileNames)
+                {
+                    listBox1.Items.Add(file);
+                }
+                listBox1.SelectedIndex = index + 1;
+            }
+        }
+
+        private void lblNombre_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Debe tener al menos 1 archivo en la lista de PDFs.", "Falta un parámetro para proceder:", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            string cPath = Path.GetDirectoryName(listBox1.Items[0].ToString());
+            string cName = Path.GetFileName(listBox1.Items[0].ToString());
+            txtnombre.Text = cPath + @"\Unido-" + cName;
+        }
+    }
+}
