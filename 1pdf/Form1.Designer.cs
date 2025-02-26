599fa54c3b4226e37158831db0fe0b7a159115b1
John Harold Belalcazar <johnharold@hotmail.com>
2025-02-25 23:48:26 -0500

    Adding Preview for PDFs!

--- 1pdf/Form1.Designer.cs
+++ 1pdf/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Windows.Forms;
+
 namespace _1pdf
 {
     partial class Form1
@@ -7,6 +9,9 @@ namespace _1pdf
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        // Add this to the designer file
+        private System.Windows.Forms.PictureBox pictureBox1;
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -29,150 +34,190 @@ namespace _1pdf
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnGenerar = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.btnSelectFiles = new System.Windows.Forms.Button();
-            this.txtnombre = new System.Windows.Forms.TextBox();
-            this.lblNombre = new System.Windows.Forms.Label();
-            this.btnArchivo = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.btnRemove = new System.Windows.Forms.Button();
-            this.btnAbajo = new System.Windows.Forms.Button();
-            this.btnArriba = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            btnGenerar = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            btnSelectFiles = new Button();
+            txtnombre = new TextBox();
+            lblNombre = new Label();
+            btnArchivo = new Button();
+            listBox1 = new ListBox();
+            btnRemove = new Button();
+            btnAbajo = new Button();
+            btnArriba = new Button();
+            pictureBox1 = new PictureBox();
+            numericUpDownPage = new NumericUpDown();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownPage).BeginInit();
+            SuspendLayout();
             // 
             // btnGenerar
             // 
-            this.btnGenerar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGenerar.Image = global::_1pdf.Properties.Resources.Document;
-            this.btnGenerar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnGenerar.Location = new System.Drawing.Point(718, 293);
-            this.btnGenerar.Name = "btnGenerar";
-            this.btnGenerar.Size = new System.Drawing.Size(150, 50);
-            this.btnGenerar.TabIndex = 0;
-            this.btnGenerar.Text = "Convertir 1 PDF";
-            this.btnGenerar.UseVisualStyleBackColor = true;
-            this.btnGenerar.Click += new System.EventHandler(this.button1_Click);
+            btnGenerar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnGenerar.Image = Properties.Resources.Document;
+            btnGenerar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btnGenerar.Location = new System.Drawing.Point(779, 413);
+            btnGenerar.Name = "btnGenerar";
+            btnGenerar.Size = new System.Drawing.Size(150, 50);
+            btnGenerar.TabIndex = 0;
+            btnGenerar.Text = "Convertir 1 PDF";
+            btnGenerar.UseVisualStyleBackColor = true;
+            btnGenerar.Click += button1_Click;
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileName = "openFileDialog1";
             // 
             // btnSelectFiles
             // 
-            this.btnSelectFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSelectFiles.Image = global::_1pdf.Properties.Resources.add;
-            this.btnSelectFiles.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSelectFiles.Location = new System.Drawing.Point(554, 12);
-            this.btnSelectFiles.Name = "btnSelectFiles";
-            this.btnSelectFiles.Size = new System.Drawing.Size(150, 50);
-            this.btnSelectFiles.TabIndex = 2;
-            this.btnSelectFiles.Text = "Seleccionar o arrastre y suelte";
-            this.btnSelectFiles.UseVisualStyleBackColor = true;
-            this.btnSelectFiles.Click += new System.EventHandler(this.button2_Click);
+            btnSelectFiles.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnSelectFiles.Image = Properties.Resources.add;
+            btnSelectFiles.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btnSelectFiles.Location = new System.Drawing.Point(615, 12);
+            btnSelectFiles.Name = "btnSelectFiles";
+            btnSelectFiles.Size = new System.Drawing.Size(150, 50);
+            btnSelectFiles.TabIndex = 2;
+            btnSelectFiles.Text = "Seleccionar o arrastre y suelte";
+            btnSelectFiles.UseVisualStyleBackColor = true;
+            btnSelectFiles.Click += button2_Click;
             // 
             // txtnombre
             // 
-            this.txtnombre.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtnombre.Location = new System.Drawing.Point(554, 94);
-            this.txtnombre.Multiline = true;
-            this.txtnombre.Name = "txtnombre";
-            this.txtnombre.Size = new System.Drawing.Size(314, 81);
-            this.txtnombre.TabIndex = 3;
+            txtnombre.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtnombre.Location = new System.Drawing.Point(615, 94);
+            txtnombre.Multiline = true;
+            txtnombre.Name = "txtnombre";
+            txtnombre.Size = new System.Drawing.Size(314, 81);
+            txtnombre.TabIndex = 3;
             // 
             // lblNombre
             // 
-            this.lblNombre.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblNombre.AutoSize = true;
-            this.lblNombre.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lblNombre.Location = new System.Drawing.Point(554, 76);
-            this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(234, 15);
-            this.lblNombre.TabIndex = 4;
-            this.lblNombre.Text = "Nombre nuevo PDF, clic AQUÍ para sugerir.";
-            this.lblNombre.Click += new System.EventHandler(this.lblNombre_Click);
+            lblNombre.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            lblNombre.AutoSize = true;
+            lblNombre.Cursor = Cursors.Hand;
+            lblNombre.Location = new System.Drawing.Point(615, 76);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new System.Drawing.Size(234, 15);
+            lblNombre.TabIndex = 4;
+            lblNombre.Text = "Nombre nuevo PDF, clic AQUÍ para sugerir.";
+            lblNombre.Click += lblNombre_Click;
             // 
             // btnArchivo
             // 
-            this.btnArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnArchivo.Location = new System.Drawing.Point(844, 181);
-            this.btnArchivo.Name = "btnArchivo";
-            this.btnArchivo.Size = new System.Drawing.Size(24, 23);
-            this.btnArchivo.TabIndex = 5;
-            this.btnArchivo.Text = "...";
-            this.btnArchivo.UseVisualStyleBackColor = true;
-            this.btnArchivo.Click += new System.EventHandler(this.btnArchivo_Click);
+            btnArchivo.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnArchivo.Location = new System.Drawing.Point(905, 181);
+            btnArchivo.Name = "btnArchivo";
+            btnArchivo.Size = new System.Drawing.Size(24, 23);
+            btnArchivo.TabIndex = 5;
+            btnArchivo.Text = "...";
+            btnArchivo.UseVisualStyleBackColor = true;
+            btnArchivo.Click += btnArchivo_Click;
             // 
             // listBox1
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(536, 334);
-            this.listBox1.TabIndex = 6;
+            listBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new System.Drawing.Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new System.Drawing.Size(597, 169);
+            listBox1.TabIndex = 6;
             // 
             // btnRemove
             // 
-            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRemove.Image = global::_1pdf.Properties.Resources.bad;
-            this.btnRemove.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRemove.Location = new System.Drawing.Point(554, 293);
-            this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(150, 50);
-            this.btnRemove.TabIndex = 7;
-            this.btnRemove.Text = "Borrar";
-            this.btnRemove.UseVisualStyleBackColor = true;
-            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            btnRemove.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnRemove.Image = Properties.Resources.bad;
+            btnRemove.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btnRemove.Location = new System.Drawing.Point(615, 293);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new System.Drawing.Size(150, 50);
+            btnRemove.TabIndex = 7;
+            btnRemove.Text = "Borrar";
+            btnRemove.UseVisualStyleBackColor = true;
+            btnRemove.Click += btnRemove_Click;
             // 
             // btnAbajo
             // 
-            this.btnAbajo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAbajo.Image = global::_1pdf.Properties.Resources.Down;
-            this.btnAbajo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnAbajo.Location = new System.Drawing.Point(554, 241);
-            this.btnAbajo.Name = "btnAbajo";
-            this.btnAbajo.Size = new System.Drawing.Size(150, 50);
-            this.btnAbajo.TabIndex = 7;
-            this.btnAbajo.Text = "Abajo";
-            this.btnAbajo.UseVisualStyleBackColor = true;
-            this.btnAbajo.Click += new System.EventHandler(this.btnAbajo_Click);
+            btnAbajo.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnAbajo.Image = Properties.Resources.Down;
+            btnAbajo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btnAbajo.Location = new System.Drawing.Point(615, 241);
+            btnAbajo.Name = "btnAbajo";
+            btnAbajo.Size = new System.Drawing.Size(150, 50);
+            btnAbajo.TabIndex = 7;
+            btnAbajo.Text = "Abajo";
+            btnAbajo.UseVisualStyleBackColor = true;
+            btnAbajo.Click += btnAbajo_Click;
             // 
             // btnArriba
             // 
-            this.btnArriba.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnArriba.Image = global::_1pdf.Properties.Resources.Up;
-            this.btnArriba.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnArriba.Location = new System.Drawing.Point(554, 188);
-            this.btnArriba.Name = "btnArriba";
-            this.btnArriba.Size = new System.Drawing.Size(150, 50);
-            this.btnArriba.TabIndex = 7;
-            this.btnArriba.Text = "Arriba";
-            this.btnArriba.UseVisualStyleBackColor = true;
-            this.btnArriba.Click += new System.EventHandler(this.btnArriba_Click);
+            btnArriba.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnArriba.Image = Properties.Resources.Up;
+            btnArriba.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btnArriba.Location = new System.Drawing.Point(615, 188);
+            btnArriba.Name = "btnArriba";
+            btnArriba.Size = new System.Drawing.Size(150, 50);
+            btnArriba.TabIndex = 7;
+            btnArriba.Text = "Arriba";
+            btnArriba.UseVisualStyleBackColor = true;
+            btnArriba.Click += btnArriba_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Location = new System.Drawing.Point(12, 188);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(597, 281);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            // 
+            // numericUpDownPage
+            // 
+            numericUpDownPage.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            numericUpDownPage.Location = new System.Drawing.Point(619, 376);
+            numericUpDownPage.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownPage.Name = "numericUpDownPage";
+            numericUpDownPage.Size = new System.Drawing.Size(73, 23);
+            numericUpDownPage.TabIndex = 8;
+            numericUpDownPage.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownPage.ValueChanged += numericUpDownPage_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Cursor = Cursors.Hand;
+            label1.Location = new System.Drawing.Point(619, 358);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(62, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Ir a página";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 364);
-            this.Controls.Add(this.btnArriba);
-            this.Controls.Add(this.btnAbajo);
-            this.Controls.Add(this.btnRemove);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.btnArchivo);
-            this.Controls.Add(this.lblNombre);
-            this.Controls.Add(this.txtnombre);
-            this.Controls.Add(this.btnSelectFiles);
-            this.Controls.Add(this.btnGenerar);
-            this.Name = "Form1";
-            this.Text = "Unirs PDFs en uno solo. ";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(941, 471);
+            Controls.Add(label1);
+            Controls.Add(numericUpDownPage);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnArriba);
+            Controls.Add(btnAbajo);
+            Controls.Add(btnRemove);
+            Controls.Add(listBox1);
+            Controls.Add(btnArchivo);
+            Controls.Add(lblNombre);
+            Controls.Add(txtnombre);
+            Controls.Add(btnSelectFiles);
+            Controls.Add(btnGenerar);
+            Name = "Form1";
+            Text = "Unirs PDFs en uno solo. ";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownPage).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -187,6 +232,8 @@ namespace _1pdf
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnAbajo;
         private System.Windows.Forms.Button btnArriba;
+        private NumericUpDown numericUpDownPage;
+        private Label label1;
     }
 }
 
